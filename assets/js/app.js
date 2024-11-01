103547a148673c87b39829430d5813d780e64364
JustinY <89873541+jyip808@users.noreply.github.com>
2024-11-01 11:03:47 -1000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#000000"
+        "value": "#9dd5ff"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#9dd5ff"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#000000",
+        "color": "#9dd5ff",
         "opacity": 0.4,
         "width": 1
       },
